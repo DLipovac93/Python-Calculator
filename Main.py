fe7d841e988f0e4d13f08b4c3ce6ec926b0d21e4
Damir Lipovac <LipovacD93@gmail.com>
2024-04-06 01:09:29 -0700

    added code

---
+++ Main.py
@@ -0,0 +1,18 @@
+operator = input("Enter a operator (+ - x /): ")
+num1 = float(input("Enter the 1st Number: "))
+num2 = float(input("Enter the 2nd Nubmer: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "x":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else :
+    print(f"{operator} is not a vaild operator")
